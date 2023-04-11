a896e37fd047ff1edecb8e09aef068523eabb1c6
Mutznch <110611081+Mutznch@users.noreply.github.com>
2023-04-11 00:15:37 -0300

    Delete src directory

--- src/main/java/br/pucpr/sportsserver/rest/discussions/replies/ReplyRepository.java
+++
@@ -1,8 +0,0 @@
-package br.pucpr.sportsserver.rest.discussions.replies;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReplyRepository extends JpaRepository<Long, Reply> {
-}
